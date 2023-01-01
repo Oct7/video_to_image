9c58c83739c43cfefeb1a291a59210bc52c8873f
oct7 <oct7.nilk@gmail.com>
2023-01-01 21:24:22 +0900

    update for several folders

--- cut_fisheye_noise.py
+++ cut_fisheye_noise.py
@@ -6,29 +6,26 @@ import os
 pathDir = 'image'
 fileList = os.listdir(pathDir)
 
+for folder in fileList:
+    fileList_second = os.listdir(pathDir+'/'+folder)
+    for image in fileList_second:
+        if image.split('.')[0] == 'jpg' and image.find('origin') != -1:
+            # Open the input image as numpy array, convert to RGB
+            img = Image.open(folder+'/'+image).convert("RGB")
+            npImage = np.array(img)
+            h, w = img.size
 
-for image in fileList:    
-    print(image)
-    if image.split('.')[0]!='':
-        # Open the input image as numpy array, convert to RGB
-        img=Image.open('image/'+image).convert("RGB")
-        npImage=np.array(img)
+            # Create same size alpha layer with circle
+            alpha = Image.new('L', img.size, 0)
+            draw = ImageDraw.Draw(alpha)
+            draw.pieslice([50, -450, h-50, w+450], 0, 360, fill=255)
 
-        h,w=img.size
+            # Convert alpha Image to numpy array
+            npAlpha = np.array(alpha)
 
-        # Create same size alpha layer with circle
-        alpha = Image.new('L', img.size,0)
-        draw = ImageDraw.Draw(alpha)
-        draw.pieslice([50,-450,h-50,w+450],0,360,fill=255)
+            # Add alpha layer to RGB
+            npImage = np.dstack((npImage, npAlpha))
 
-        # Convert alpha Image to numpy array
-        npAlpha=np.array(alpha)
-
-        # Add alpha layer to RGB
-        npImage=np.dstack((npImage,npAlpha))
-
-        # Save with alpha
-        Image.fromarray(npImage[:,50:h-50]).save('result/'+image.split('.')[0]+'.png')
-
-
-
+            # Save with alpha
+            Image.fromarray(
+                npImage[:, 50:h-50]).save(pathDir+'/'+folder+'/'+image.split('.')[0]+'.png')
