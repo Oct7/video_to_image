19ab87bf2de2655bf09ebb7ae08779da71006a7e
oct7 <oct7.nilk@gmail.com>
2023-01-17 11:23:40 +0900

    update

--- panorama_splitter.py
+++ panorama_splitter.py
@@ -3,25 +3,27 @@ import os
 from tkinter import simpledialog
 import tkinter as tk
 import time
- 
- 
+
+
 def dialog(text):
     ROOT = tk.Tk()
     ROOT.withdraw()
     USER_INP = simpledialog.askstring(title="저장 폴더명",
-                                    prompt=text)
+                                      prompt=text)
     return USER_INP
- 
-pathDir = 'image' 
+
+
+pathDir = 'image'
 fileList = os.listdir(pathDir)
 cutNum = dialog('자를 갯수를 입력하세요.')
-for image in fileList:    
+for image in fileList:
     print(image)
-    if image.split('.')[0]!='':
+    if image.split('.')[0] != '':
         # 인풋 image, 세로컷수, 가로컷수
-        if os.path.isdir('image'+'/%s'%image.split('.jpg')[0]+'/image'+'/%s'%image.split('.jpg')[0])==False:
-            os.makedirs('image'+'/%s'%image.split('.jpg')[0]+'/image'+'/%s'%image.split('.jpg')[0])
+        if os.path.isdir('image'+'/%s' % image.split('.jpg')[0]+'/image'+'/%s' % image.split('.jpg')[0]) == False:
+            os.makedirs('image'+'/%s' % image.split('.jpg')
+                        [0]+'/image'+'/%s' % image.split('.jpg')[0])
             time.sleep(1)
-        
-        split_image("image/"+image, cutNum, 1, False,False,output_dir='image'+'/%s'%image.split('.jpg')[0])
 
+        split_image("image/"+image, int(cutNum), 1, False, False,
+                    output_dir='image'+'/%s' % image.split('.jpg')[0])
